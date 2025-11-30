fc0a250e523d3b7ed5fc60c0b2f7ee13bc4bf7a7
deayulianis <deayulianis788@gmail.com>
2025-11-30 22:15:37 +0700

    cahyo commit

---
+++ pert3_50422441/src/main/java/com/mycompany/pert3_50422441/Pert3_50422441.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422441;
+
+/**
+ *
+ * @author dwica
+ */
+public class Pert3_50422441 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
